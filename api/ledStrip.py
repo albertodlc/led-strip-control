bf5bc720a933183ccd866843553fdaae5d7c2d72
albertodlc <albertodelacruz96@gmail.com>
2020-10-21 17:51:28 +0200

    API

--- api/ledStrip.py
+++ api/ledStrip.py
@@ -80,76 +80,62 @@ class Utils():
         return '{:02x}{:02x}{:02x}'.format(R, G, B)
 
 class DeviceControl:
-    def __init__(self, mac_addr):
+    #def __init__(self, mac_addr):
         # Init LED STRIP with white color
-        self.R = 255
-        self.G = 255
-        self.B = 255
+        #self.R = 255
+        #self.G = 255
+        #self.B = 255
 
-        self.mac_addr = mac_addr
+        #self.mac_addr = mac_addr
 
-        self.p = Peripheral(self.mac_addr)
+        #self.p = Peripheral(self.mac_addr)
 
-        self.s = self.p.getServiceByUUID(LED_SERVICES[4])
-        self.ch_W = self.s.getCharacteristics(LED_CHARACTERISTICS[0])[0]
-        self.ch_W.write(LedStripMessages.color_message(self.R, self.G, self.B))
+        #self.s = self.p.getServiceByUUID(LED_SERVICES[4])
+        #self.ch_W = self.s.getCharacteristics(LED_CHARACTERISTICS[0])[0]
+        #self.ch_W.write(LedStripMessages.color_message(self.R, self.G, self.B))
 
-    def show_perif_services_char(self):
-        print("\n\tDevice:" + self.p.addr)
-        print("\t\t", end = '')
-        print(self.s)
-        print("\t\t\t", end = '')
-        print(self.ch)
+    #def show_perif_services_char(self):
+    #    print("\n\tDevice:" + self.p.addr)
+    #    print("\t\t", end = '')
+    #    print(self.s)
+    #    print("\t\t\t", end = '')
+    #    print(self.ch)
 
-    def show_perif(self):
-        print("\nDevice: " + self.p.addr)
+    #def show_perif(self):
+    #    print("\nDevice: " + self.p.addr)
 
-    def show_services(self):
-        print("Services:")
-        for s in self.p.getServices():
-            print("\tUUID", s.uuid.getCommonName(), s.uuid.binVal)
+    #def show_services(self):
+    #    print("Services:")
+    #    for s in self.p.getServices():
+    #        print("\tUUID", s.uuid.getCommonName(), s.uuid.binVal)
 
-    def show_charac(self):
-        print("Characteristics")
-        for s in self.p.getServices():
-            for ch in s.getCharacteristics():
-                print("\tUUID", ch.uuid.getCommonName(), s.uuid.binVal)
+    #def show_charac(self):
+    #    print("Characteristics")
+    #    for s in self.p.getServices():
+    #        for ch in s.getCharacteristics():
+    #            print("\tUUID", ch.uuid.getCommonName(), s.uuid.binVal)
 
     @app.route('/on', methods=['POST'])
-    def turn_on(self):
-        self.ch_W.write(LedStripMessages.on_message())
-
+    def turn_on():
+        mac_addr = "52:14:00:00:C6:A9"
+        p = Peripheral(mac_addr)
+        s = p.getServiceByUUID(LED_SERVICES[4])
+        ch_W = s.getCharacteristics(LED_CHARACTERISTICS[0])[0]
+        ch_W.write(LedStripMessages.on_message())
+        p.disconnect()
+        
     @app.route('/off', methods=['POST'])
-    def turn_off(self):
-        self.ch_W.write(LedStripMessages.off_message())
-
-    def turn_color(self, R, G, B):
-        self.R = R
-        self.G = G
-        self.B = B
-
-        self.ch_W.write(LedStripMessages.color_message(R, G, B))
-
-    def turn_intensity(self, intensity):
-        ## Under development, we need to sniff and see how it work DON'T USE THIS
-        aux = 1.0
-
-        if intensity > 1.0:
-            aux = 1.0
-        elif intensity < 0.0:
-            aux = 0.0
-        else:
-            aux = intensity
-        self.ch_W.write(LedStripMessages.color_message(int(aux*self.R), int(aux*self.G), int(aux*self.B)))
-
-    def close_connection(self):
-        self.p.disconnect()
+    def turn_off():
+        mac_addr = "52:14:00:00:C6:A9"
+        p = Peripheral(mac_addr)
+        s = p.getServiceByUUID(LED_SERVICES[4])
+        ch_W = s.getCharacteristics(LED_CHARACTERISTICS[0])[0]
+        ch_W.write(LedStripMessages.off_message())
+        p.disconnect()
 
 #sd = ScanDevices()
 
 #for dev in sd.device_array:
 #    dc = DeviceControl(dev)
 #    dc.close_connection()
-
-dc = DeviceControl("52:14:00:00:C6:A9")
 app.run()
