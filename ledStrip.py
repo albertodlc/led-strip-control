ecbe904019ba483ef5abf3e898c52b89874e3ea8
albertodlc <albertodelacruz96@gmail.com>
2020-10-21 15:36:53 +0200

    Test

--- ledStrip.py
+++ ledStrip.py
@@ -18,7 +18,7 @@ LED_CHARACTERISTICS = [UUID(0xFFD9)]
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
-        
+
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device", dev.addr)
@@ -29,20 +29,20 @@ class ScanDevices:
     def __init__(self, sd = None, scan_time = 10.0):
         self.scan_time = scan_time
         self.device_array = []
-        
+
         if sd == None:
             self.sc = Scanner() # Init without HandleDiscovery
         else:
             self.sc = sc = Scanner().withDelegate(sd) # Init with HandleDiscovery
-        
+
         # Scan for devices and return a list of ScanEntry
         scan_entry_array = self.sc.scan(self.scan_time)
-        
+
         # Check if any of the detected devices is a LED STRIP
         for se in scan_entry_array:
             if any(word in se.addr.upper() for word in MAC_INIT):
                 self.device_array.append(se.addr.upper())
-        
+
     def show_devices(self):
         print("**AVAILABLE LED STRIPS**\n")
         for dev in self.device_array:
@@ -54,74 +54,74 @@ class LedStripMessages:
     FINAL_HEX = "00F0AA"
     TURN_ON = "CC2333"
     TURN_OFF = "CC2433"
-    
+
     @staticmethod
     def on_message():
         return bytearray.fromhex(TURN_ON)
-    
+
     @staticmethod
     def off_message():
         return bytearray.fromhex(TURN_OFF)
-    
+
     @staticmethod
     def color_message(R,G,B):
         return bytearray.fromhex(LedStripMessages.FIRST_HEX + Utils.rgb_to_hex(R, G, B) + LedStripMessages.FINAL_HEX)
-    
+
 class Utils():
     @staticmethod
     def rgb_to_hex(R, G, B):
         return '{:02x}{:02x}{:02x}'.format(R, G, B)
-    
+
 
 class DeviceControl:
     def __init__(self, mac_addr):
         # Init LED STRIP with white color
         self.mac_addr = mac_addr
-        
+
         self.p = Peripheral(self.mac_addr)
-        
+
         self.s = self.p.getServiceByUUID(LED_SERVICES[4])
-        self.ch_W = self.s.getCharacteristics(LED_CHARACTERISTICS[0])[0]   
+        self.ch_W = self.s.getCharacteristics(LED_CHARACTERISTICS[0])[0]
         self.ch_W.write(LedStripMessages.color_message(self.R, self.G, self.B))
-        
+
     def show_perif_services_char(self):
         print("\n\tDevice:" + self.p.addr)
         print("\t\t", end = '')
         print(self.s)
         print("\t\t\t", end = '')
         print(self.ch)
-    
+
     def show_perif(self):
         print("\nDevice: " + self.p.addr)
-    
+
     def show_services(self):
         print("Services:")
         for s in self.p.getServices():
             print("\tUUID", s.uuid.getCommonName(), s.uuid.binVal)
-    
+
     def show_charac(self):
         print("Characteristics")
         for s in self.p.getServices():
             for ch in s.getCharacteristics():
-                print("\tUUID", ch.uuid.getCommonName(), s.uuid.binVal)       
-    
+                print("\tUUID", ch.uuid.getCommonName(), s.uuid.binVal)
+
     def turn_on(self):
         self.ch_W.write(LedStripMesages.on_message())
-        
-    def turn_off(self):        
+
+    def turn_off(self):
         self.ch_W.write(LedStripMesages.off_message())
-    
+
     def turn_color(self, R, G, B):
         self.R = R
         self.G = G
         self.B = B
-        
+
         self.ch_W.write(LedStripMessages.color_message(R, G, B))
-    
+
     def turn_intensity(self, intensity):
-        ## Under development, we need to sniff and see how it work
+        ## Under development, we need to sniff and see how it work DON'T USE THIS
         aux = 1.0
-        
+
         if intensity > 1.0:
             aux = 1.0
         elif intensity < 0.0:
@@ -129,7 +129,7 @@ class DeviceControl:
         else:
             aux = intensity
         self.ch_W.write(LedStripMessages.color_message(int(aux*self.R), int(aux*self.G), int(aux*self.B)))
-    
+
     def close_connection(self):
         self.p.disconnect()
 
@@ -143,9 +143,12 @@ dc = DeviceControl("52:14:00:00:C6:A9")
 dc.turn_off()
 dc.close_connection()
 
-
-            
-        
-        
-
-
+loop = True
+while(loop):
+    opc = input()
+    if opc == 0:
+        print("Turn on light")
+        dc.turn_on()
+    elif opc == 1:
+        print("Turn off light")
+        dc.turn_off()
