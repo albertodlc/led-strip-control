cd1a4ab2992399a5c57c02f593134da5a8488195
albertodlc <albertodelacruz96@gmail.com>
2020-10-27 22:04:15 +0100

    Test

--- ledStrip.py
+++ ledStrip.py
@@ -88,7 +88,7 @@ class DeviceControl:
         self.ch_N = self.s_R.getCharacteristics(LED_CHARACTERISTICS[1])[0]
 
     def notifications(self):
-        print(self.ch_N.read())
+        print(self.ch_N.read().hex())
         #self.p.waitForNotifications(5.0) # Wait for 5 second
 
         #self.ch_N.write(bytearray.fromhex("01"))
