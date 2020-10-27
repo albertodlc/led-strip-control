69433e08e301a018cb788443e6ae05fa0a953031
albertodlc <albertodelacruz96@gmail.com>
2020-10-27 19:56:27 +0100

    v1.0

--- ledStrip.py
+++ ledStrip.py
@@ -63,7 +63,7 @@ class DeviceControl:
         self.p = Peripheral(self.led_status["MAC"])
         self.s_W = self.p.getServiceByUUID(LED_SERVICES[4])
         self.ch_W = self.s_W.getCharacteristics(LED_CHARACTERISTICS[0])[0]
-        self.ch_N = self.s_W.getCharacteristics(LED_CHARACTERISTICS[1])[0]
+        self.ch_N = self.s_W.getCharacteristics(LED_CHARACTERISTICS[0])[1]
 
     def notifications(self):
         self.ch_W.write(bytearray.fromhex("EF"))
